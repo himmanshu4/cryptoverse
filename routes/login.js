b4526050058262d423980fc2aa44691b6178c5ae
himanshu <himmanshumishra4@gmail.com>
2023-08-04 00:47:49 +0530

    implement return to previous page after login

--- routes/login.js
+++ routes/login.js
@@ -5,5 +5,15 @@ const loginRouter = Router();
 loginRouter.get('/', function (req, res) {
     res.render("login")
 })
-loginRouter.post("/",passport.authenticate("local",{failureRedirect:"/login",successRedirect:"/"}))
-module.exports=loginRouter;
+loginRouter.post("/",
+    (req, res, next) => {
+        console.log(req.cookies)
+        next()
+    },
+    passport.authenticate("local", { failureRedirect: "/login", keepSessionInfo: true }),
+    (req, res) => {
+        console.log(req.cookies)
+        let url = req.cookies.afterLogin ?? '/';
+        res.redirect(url);
+    })
+module.exports = loginRouter;
