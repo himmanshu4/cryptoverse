9b4110be20533612d9e706f500b188ad3dddb75e
himanshu <himmanshumishra4@gmail.com>
2023-08-04 19:36:59 +0530

    added option to delete account

--- routes/logout.js
+++ routes/logout.js
@@ -1,10 +1,24 @@
 const { Router } = require("express");
 const passport = require("passport");
-
+const User = require("../models/user");
+const { Transaction } = require("../models/transaction");
+const debug = require('debug')('cryptoverse:logout')
 const logoutRouter = Router();
-logoutRouter.get('/', function (req, res) {
-    req.logOut()
-    res.redirect('/');
+logoutRouter.post('/', function (req, res,next) {
+    if (req.isAuthenticated()) {
+        req.logOut((err)=>err&&debug(err))
+        res.send('logged out');
+    }
+    else res.send('Not Logged In');
 })
-
-module.exports=logoutRouter;
+logoutRouter.post('/forever', function (req, res,next) {
+    if (req.isAuthenticated()) {
+        const id = req.user._id;
+        Transaction.deleteMany({user:id}).exec();
+        User.findByIdAndDelete(id).exec();
+        req.logOut((err)=>err&&debug(err))
+        res.send('Account deleted');
+    }
+    else res.send('Not Logged In');
+})
+module.exports = logoutRouter;
