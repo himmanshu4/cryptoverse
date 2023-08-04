0b03ed9ab65f4aa75b8d312d5358428583d39389
himanshu <himmanshumishra4@gmail.com>
2023-08-04 20:07:55 +0530

    added various endpoints

---
+++ routes/api.js
@@ -0,0 +1,27 @@
+const { Router } = require("express");
+const { Transaction } = require("../models/transaction");
+const async = require("hbs/lib/async");
+const cryptodata = require("../helper/coingecko");
+
+const apiRouter = Router();
+const secureEndpoints = ["/transactions", "/rates"]
+secureEndpoints.forEach(route => apiRouter.use(route, (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next()
+    }
+    else {
+        res.status(401).send("Unauthorized");
+    }
+}))
+apiRouter.get("/transactions", async (req, res) => {
+    const transactions = await Transaction.find({ user: req.user.id });
+    res.json(transactions);
+});
+apiRouter.get("/portfolio", (req, res) => {
+    const coinBal = req.user.coinBalance;
+    res.json({ balance: req.user.balance, coinBal });
+})
+apiRouter.get("/rates", async (req, res) => {
+    res.json(cryptodata.rates);
+})
+module.exports = apiRouter;
--- routes/index.js
+++ routes/index.js
@@ -7,24 +7,26 @@ const transactionRouter = require('./transaction');
 const loginRouter = require('./login');
 const signupRouter = require('./signup');
 const logoutRouter = require('./logout');
+const apiRouter = require('./api');
 /* GET home page. */
-indexRouter.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express'});
+indexRouter.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-indexRouter.get('/markets',function (req,res,next) {
-  res.render('markets',{title: 'Markets'})
+indexRouter.get('/markets', function (req, res, next) {
+  res.render('markets', { title: 'Markets' })
 })
-indexRouter.get('/chart',(req,res)=>{
-  res.render('chart',{title: 'Charts'})
+indexRouter.get('/chart', (req, res) => {
+  res.render('chart', { title: 'Charts' })
 })
-indexRouter.get("/profile",(req,res)=>{
-  res.render("profile",{user:req.user})
+indexRouter.get("/profile", (req, res) => {
+  res.render("profile", { user: req.user })
 })
-indexRouter.use('/login',loginRouter);
-indexRouter.use('/logout',logoutRouter);
-indexRouter.use('/signup',signupRouter);
-indexRouter.use('/users/',usersRouter);
-indexRouter.use('/rates/',ratesRouter);
-indexRouter.use('/transaction',transactionRouter);
+indexRouter.use('/login', loginRouter);
+indexRouter.use('/logout', logoutRouter);
+indexRouter.use('/signup', signupRouter);
+indexRouter.use('/users/', usersRouter);
+indexRouter.use('/rates/', ratesRouter);
+indexRouter.use('/transaction', transactionRouter);
+indexRouter.use("/api", apiRouter)
 module.exports = indexRouter;
